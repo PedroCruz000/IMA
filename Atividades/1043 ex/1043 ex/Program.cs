9c6e060046d695f0ab941bbcd87a2a99db56ee90
PedroCruz000 <112138392+PedroCruz000@users.noreply.github.com>
2022-12-04 15:21:04 -0300

    1043

---
+++ Atividades/1043 ex/1043 ex/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ex1043
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string linha = Console.ReadLine();
+            string[] numerosABC = linha.Split();
+
+            double A = double.Parse(numerosABC[0]);
+            double B = double.Parse(numerosABC[1]);
+            double C = double.Parse(numerosABC[2]);
+
+
+            Console.WriteLine(A);
+            Console.WriteLine(B);
+            Console.WriteLine(C);   
+        }
+    }
+}
