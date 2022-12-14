fc448fd4a300301bb6ef645ef4b8a2ec5f4e3070
PedroCruz000 <112138392+PedroCruz000@users.noreply.github.com>
2022-12-13 22:04:41 -0300

    fim dos exercicios

---
+++ Atividades/1031 ex/1031 ex/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace ex1131
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int totalVitorisInter = 0;
+            int totalVitoriasGremio = 0;
+            int totalDeEmpates = 0;
+            int numeroDeGrenais = 0;
+            while(true)
+            {
+                string linha = Console.ReadLine();
+                string[] golsInterEGremio = linha.Split();
+                
+                int golInter = int.Parse(golsInterEGremio[0]);
+                int golGremio = int.Parse((golsInterEGremio[1]));
+
+                if (golInter > golGremio)
+                {
+                    totalVitorisInter++;
+                }
+                else if (golInter < golGremio)
+                {
+                    totalVitoriasGremio++;
+                }
+                else
+                {
+                    totalDeEmpates++;
+                }
+
+                Console.WriteLine("Novo grenal (1-sim 2-nao)");
+                int continuar = int.Parse(Console.ReadLine());
+
+                numeroDeGrenais++;
+
+                if (continuar ==2 )
+                {
+                    Console.WriteLine($"{numeroDeGrenais} grenais");
+                    Console.WriteLine($"Inter:{totalVitorisInter}");
+                    Console.WriteLine($"Gremio:{totalVitoriasGremio}");
+                    Console.WriteLine($"Empates:{totalDeEmpates}");
+                    if (totalVitorisInter > totalVitoriasGremio)
+                    {
+                        Console.WriteLine("Inter venceu mais");
+                    }
+                    else if (totalVitorisInter < totalVitoriasGremio)
+                    {
+                        Console.WriteLine("Gremio venceu mais");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Não houve vencedor");
+                    }
+                    break;
+                }
+              
+
+
+            }
+           
+
+        }
+    }
+}
---
+++ Atividades/1132 ex/1132 ex/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace ex1132
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int somaDosDivisiveis = 0;
+            if (x > y)
+            {
+                for (int i =y; i<=x; i++)
+                {
+                    if (i % 13 != 0)
+                    {
+                        somaDosDivisiveis = somaDosDivisiveis + i;
+
+                    }
+                }
+            }
+            else
+            {
+                for (int i = x; i <= y; i++)
+                {
+                    if (i % 13 != 0)
+                    {
+                        somaDosDivisiveis = somaDosDivisiveis + i;
+
+                    }
+                }
+
+            }
+          
+            Console.WriteLine(somaDosDivisiveis);
+
+
+        }
+    }
+}
