b73346bd119c557dcd9d7a80a1c928d85cb66db3
PedroCruz000 <112138392+PedroCruz000@users.noreply.github.com>
2023-01-05 01:40:37 -0300

    projeto do banco funcionando

--- Atividades/projeto banco 2/projeto banco 2/Program.cs
+++ Atividades/projeto banco 2/projeto banco 2/Program.cs
@@ -1,60 +1,55 @@
-﻿namespace projetoBanco2
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace ByteBank1
 {
+
     public class Program
     {
-        // MENU
-        static void MostrarOMenu()
+
+        static void ShowMenu()
         {
-            Console.WriteLine("1 - Inserir novo Usuário");
-            Console.WriteLine("2 - Deletar um Usuário");
+            Console.WriteLine("1 - Inserir novo usuário");
+            Console.WriteLine("2 - Deletar um usuário");
             Console.WriteLine("3 - Listar todas as contas registradas");
-            Console.WriteLine("4 - Detalhes do Usuário");
-            Console.WriteLine("5 - Saldo");
+            Console.WriteLine("4 - Detalhes de um usuário");
+            Console.WriteLine("5 - Quantia armazenada no banco");
             Console.WriteLine("6 - Manipular a conta");
-            Console.WriteLine("0 - sair");
-            Console.Write("Digite a opção desejada:");
+            Console.WriteLine("0 - Para sair do programa");
+            Console.Write("Digite a opção desejada: ");
         }
 
-        // REGISTRAR UM USUARIO
-        static void RegistrarNovoUsuario(List<string> cpfs, List<string> titulares,List<string> senhas, List<double> saldos)
+        static void RegistrarNovoUsuario(List<string> cpfs, List<string> titulares, List<string> senhas, List<double> saldos)
         {
-            Console.Write("Digite o Cpf:");
+            Console.Write("Digite o cpf: ");
             cpfs.Add(Console.ReadLine());
-
-            Console.Write("Digite o nome:");
+            Console.Write("Digite o nome: ");
             titulares.Add(Console.ReadLine());
-
-            Console.Write("Digite a Senha:");
+            Console.Write("Digite a senha: ");
             senhas.Add(Console.ReadLine());
-
             saldos.Add(0);
         }
 
-        // DELETAR UM USUÁRIO 
-
         static void DeletarUsuario(List<string> cpfs, List<string> titulares, List<string> senhas, List<double> saldos)
         {
-            Console.Write("Digite o cpf:");
+            Console.Write("Digite o cpf: ");
             string cpfParaDeletar = Console.ReadLine();
-            int indexParaDeletar = cpfs.FindIndex( d=> d == cpfParaDeletar);
-           
+            int indexParaDeletar = cpfs.FindIndex(cpf => cpf == cpfParaDeletar);
 
             if (indexParaDeletar == -1)
             {
-                Console.WriteLine("Não foi possivel deletar esta conta");
-                Console.WriteLine("Motivo: conta não encontrada.");
+                Console.WriteLine("Não foi possível deletar esta Conta");
+                Console.WriteLine("MOTIVO: Conta não encontrada.");
             }
-            
+
             cpfs.Remove(cpfParaDeletar);
             titulares.RemoveAt(indexParaDeletar);
             senhas.RemoveAt(indexParaDeletar);
             saldos.RemoveAt(indexParaDeletar);
 
             Console.WriteLine("Conta deletada com sucesso");
-
-
         }
-        static void listarTodasAsContas(List<string> cpfs, List<string> titulares, List<double> saldos)
+
+        static void ListarTodasAsContas(List<string> cpfs, List<string> titulares, List<double> saldos)
         {
             for (int i = 0; i < cpfs.Count; i++)
             {
@@ -64,77 +59,69 @@
 
         static void ApresentarUsuario(List<string> cpfs, List<string> titulares, List<double> saldos)
         {
-            Console.Write("Digite o cpf:");
+            Console.Write("Digite o cpf: ");
             string cpfParaApresentar = Console.ReadLine();
             int indexParaApresentar = cpfs.FindIndex(cpf => cpf == cpfParaApresentar);
 
-
             if (indexParaApresentar == -1)
             {
-                Console.WriteLine("Não foi possivel apresentar esta conta");
-                Console.WriteLine("Motivo: conta não encontrada.");
+                Console.WriteLine("Não foi possível apresentar esta Conta");
+                Console.WriteLine("MOTIVO: Conta não encontrada.");
             }
 
             ApresentaConta(indexParaApresentar, cpfs, titulares, saldos);
         }
 
-        static double soma(List<double> saldos)
+        static void ApresentarValorAcumulado(List<double> saldos)
         {
-            return saldos.Aggregate(0.0,(x,y) => x + y);
+            Console.WriteLine($"Total acumulado no banco: {saldos.Sum()}");
+            // saldos.Sum(); ou .Agregatte(0.0, (x, y) => x + y)
         }
-        //não sei o que é isso 
-      
 
-
-        static void ApresentaConta(int index , List<string> cpfs, List<string> titulares, List<double> saldos, int i)
+        static void ApresentaConta(int index, List<string> cpfs, List<string> titulares, List<double> saldos)
         {
-            Console.WriteLine($"CPF = {cpfs[index]} | Titular {titulares[index]} | Saldos R$ {saldos[index]:F2}");
+            Console.WriteLine($"CPF = {cpfs[index]} | Titular = {titulares[index]} | Saldo = R${saldos[index]:F2}");
         }
 
         public static void Main(string[] args)
         {
-            Console.WriteLine(" Antes de começão a usar, vamos configurar alguns valores:");
+
+            Console.WriteLine("Antes de começar a usar, vamos configurar alguns valores: ");
 
             List<string> cpfs = new List<string>();
             List<string> titulares = new List<string>();
             List<string> senhas = new List<string>();
             List<double> saldos = new List<double>();
 
-
-
-
             int option;
 
             do
             {
-                MostrarOMenu();
+                ShowMenu();
                 option = int.Parse(Console.ReadLine());
 
-                Console.WriteLine("------------------------");
-                //REALIZAR OPÇÃO DESEJADA
+                Console.WriteLine("-----------------");
 
                 switch (option)
                 {
                     case 0:
-                        Console.WriteLine("Encerrando o Programa...");
+                        Console.WriteLine("Estou encerrando o programa...");
                         break;
-                    case 1: 
-                        RegistrarNovoUsuario(cpfs, titulares,senhas,saldos);
+                    case 1:
+                        RegistrarNovoUsuario(cpfs, titulares, senhas, saldos);
                         break;
                     case 2:
                         DeletarUsuario(cpfs, titulares, senhas, saldos);
                         break;
                     case 3:
-                        listarTodasAsContas(cpfs, titulares, saldos);
+                        ListarTodasAsContas(cpfs, titulares, saldos);
                         break;
                     case 4:
-                        apresentarUsuario(cpfs, titulares, saldos);
+                        ApresentarUsuario(cpfs, titulares, saldos);
                         break;
-
                 }
-         
 
-
+                Console.WriteLine("-----------------");
 
             } while (option != 0);
 
@@ -142,6 +129,6 @@
 
         }
 
-     
     }
+
 }
