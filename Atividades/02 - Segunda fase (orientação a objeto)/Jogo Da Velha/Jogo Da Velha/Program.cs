a2d3edab5bf6433d0d3c92ef2ce3c335ad6dd7cf
PedroCruz000 <112138392+PedroCruz000@users.noreply.github.com>
2023-01-17 22:13:08 -0300

    jogo da velha finalizado

--- Atividades/02 - Segunda fase (orientação a objeto)/Jogo Da Velha/Jogo Da Velha/Program.cs
+++ Atividades/02 - Segunda fase (orientação a objeto)/Jogo Da Velha/Jogo Da Velha/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace JogoDaVelha
+﻿using System.Numerics;
+
+namespace JogoDaVelha
 {
     class Program
     {
@@ -7,7 +9,7 @@
             string[,] matriz = new string[3, 3];
 
             string turno = "X";
-            int tentativas = 0;
+            int tentativas = 1;
             
 
             //lista para adicionar as jogadas feitas, e não deixar o usuario repetilas 
@@ -100,28 +102,90 @@
 
                 }
 
+                //condição de vitoria 
+
+                // vitoria nas diagonais 
+
+                if (matriz[0,0] == matriz[1,1] && matriz[0,0] == matriz[2,2] ||
+                    matriz[0,2] == matriz[1,1] && matriz[0,2] == matriz[2,0])
+                {
+
+                    Console.WriteLine("");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"O JOGADOR:----{turno}---- VENCEU ");
+                    Console.ResetColor();
+                    break;
+                }
+
+                //vitoria nas horizontais 
+
+                if (matriz[0,0] == matriz[0,1] && matriz[0,0] == matriz[0,2]||
+                    matriz[1,0] == matriz[1,1] && matriz[1,0] == matriz[1,2]||
+                    matriz[2,0] == matriz[2,1] && matriz[2,0] == matriz[2,2])
+                {
+                    Console.WriteLine("");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"O JOGADOR:----{turno}---- VENCEU ");
+                    Console.ResetColor();
+                    break;
+                } 
+
+                //vitorias nas verticais 
+
+                if (matriz[0,0] == matriz[1,0] && matriz[0,0] == matriz[2,0]||
+                    matriz[0,1] == matriz[1,1] && matriz[0,1] == matriz[2,1]||
+                    matriz[0,2] == matriz[1,2] && matriz[0,2] == matriz[2,2])
+                {
+                    Console.WriteLine("");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"O JOGADOR:----{turno}---- VENCEU ");
+                    Console.ResetColor();
+                    break;
+                }
+
                 // escrever a proxima jogada 
+
                 Console.WriteLine("");
                 Console.Write("Digite a posição da sua jogada: ");
                 jogada = Console.ReadLine();
 
                 if (turno == "X")
                 {
+                    
                     turno = "O";
-
+                    
                 }
                 else
                 {
                     turno = "X";
                 }
 
-                tentativas++;
+
+                //erro caso digite uma tecla errada 
 
                 while (!indexNumeros.Contains(jogada))
                 {
+                    Console.ForegroundColor = ConsoleColor.Red;
                     Console.Write("Jogada invalida. Tente Novamente");
+                    Console.ResetColor();
+
                     jogada = Console.ReadLine();
                 }
+
+                tentativas++;
+
+                if (tentativas == 9)
+                {
+                    Console.WriteLine("");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Console.WriteLine($"-------DEU VELHA-------");
+                    Console.WriteLine($"-----NINGUÉM VENCEU----");
+                    Console.ResetColor();
+                    break;
+                    
+
+                }
+
                 Console.Clear() ;
 
                 
