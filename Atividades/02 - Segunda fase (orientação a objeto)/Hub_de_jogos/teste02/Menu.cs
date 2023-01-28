9ac20bc09512a4768baa8691f279c06dc9655ee1
PedroCruz000 <112138392+PedroCruz000@users.noreply.github.com>
2023-01-28 02:41:54 -0300

    hub atualizado com opções de voltar e novos menus

---
+++ Atividades/02 - Segunda fase (orientação a objeto)/Hub_de_jogos/teste02/Menu.cs
@@ -0,0 +1,259 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using teste02.Repositorio;
+
+namespace teste02
+{
+    internal class Menu
+    {
+        public static void Abertura()
+        {
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("\n\tBem-vindo(a) ao Jogo da Velha.");
+            Console.Write("\n\tPressione qualquer tecla para acessar ao menu ");
+            Console.ReadKey();
+            Console.ResetColor();
+        }
+
+        public static string PegarNome()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("\n\t Digite Seu Nome:");
+            Console.ResetColor();
+            string nome = Console.ReadLine();           
+            return nome;
+        }
+
+        public static void ShowMenu()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+            Console.WriteLine("\n\n\t 1 - Cadastrar jogador");
+            Console.WriteLine("\n\t 2 - Deletar jogador");
+            Console.WriteLine("\n\t 3 - Ranking dos jogadores");
+            Console.WriteLine("\n\t 4 - Iniciar jogo da velha");
+            Console.WriteLine("\n\t 0 - Sair");
+
+            Console.ResetColor();
+        }
+
+        public static string ExisteJogador(string nome)
+        {
+            int escolha = 10;
+
+            if(Json.jogadores.Any(player => player.Nome == nome))
+            {
+                do
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("\n\n\t ESTE JOGADOR JA EXITE! ");
+                    Console.ForegroundColor= ConsoleColor.DarkYellow;
+                    Console.WriteLine("\n\n\t 1 - Tentar novamente");
+                    Console.WriteLine("\n\t 0 - Sair");
+                    Console.ResetColor();
+
+
+                    try
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.Write("\n\t Digite uma opção:");
+                        Console.ResetColor();
+                        escolha = int.Parse(Console.ReadLine());
+                        if (escolha < 0 || escolha > 5)
+                        {
+                            Console.Clear();
+                            Console.ForegroundColor = ConsoleColor.DarkYellow;
+                            Console.WriteLine("\n\t Opção inválida, tente novamente!");
+                            Console.WriteLine("\n\t Precione uma tecla para tentar novamente!");
+                            Console.ReadKey();
+                            Console.ResetColor();
+
+                        }
+                    }
+                    catch
+                    {
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.WriteLine("\n\t Opção inválida, tente novamente!");
+                        Console.WriteLine("\n\t Precione uma tecla para tentar novamente!");
+                        Console.ReadKey();
+                        Console.ResetColor();
+
+                    }
+
+                    switch (escolha)
+                    {
+                        case 1:
+                            Json.Adicionar();
+                            escolha = 0;
+                            break;
+
+                        case 0:
+                            
+                            break;
+                    }
+
+                } while (escolha != 0);
+                
+            }
+            return nome;
+
+
+
+
+            /*
+            while (Json.jogadores.Any(player => player.Nome == nome))
+            {
+                Console.WriteLine("\n\tJogador já cadastrado. Tente novamente.");
+
+                
+                nome = PegarNome();
+            }
+            return nome;*/
+        }
+        public static string NaoExisteJogador(string nome)
+        {
+            int escolha = 10;
+
+            if (!Json.jogadores.Any(player => player.Nome == nome))
+            {
+                do
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("\n\n\t ESTE JOGADOR NÃO EXITE! ");
+                    Console.ResetColor();
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine("\n\n\t 1 - Tentar novamente");
+                    Console.WriteLine("\n\t 0 - Sair");
+                    Console.ResetColor();
+
+
+
+                    try
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.Write("\n\t Digite uma opção:");
+                        Console.ResetColor();
+                        escolha = int.Parse(Console.ReadLine());
+                        if (escolha < 0 || escolha > 5)
+                        {
+                            Console.Clear();
+                            Console.ForegroundColor = ConsoleColor.DarkYellow;
+                            Console.WriteLine("\n\t Opção inválida, tente novamente!");
+                            Console.WriteLine("\n\t Precione uma tecla para tentar novamente!");
+                            Console.ReadKey();
+                            Console.ResetColor();
+
+                        }
+                    }
+                    catch
+                    {
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.WriteLine("\n\t Opção inválida, tente novamente!");
+                        Console.WriteLine("\n\t Precione uma tecla para tentar novamente!");
+                        Console.ReadKey();
+                        Console.ResetColor();
+
+                    }
+
+                    switch (escolha)
+                    {
+                        case 1:
+                            Console.Write("\n\t Digite um nome valido:");
+                            nome = Console.ReadLine();
+                            NaoExisteJogador(nome);
+                            escolha = 0;
+                            break;
+
+                        case 0:
+                            
+                            break;
+                    }
+
+                } while (escolha != 0);
+
+            }
+        
+            return nome;
+        }
+
+       
+
+        public static string NaoExisteJogadorParaDeletar(string nome)
+        {
+            int escolha = 10;
+
+            if (!Json.jogadores.Any(player => player.Nome == nome))
+            {
+                do
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("\n\n\t ESTE JOGADOR NÃO EXITE! ");
+                    Console.ResetColor();
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine("\n\n\t 1 - Tentar novamente");
+                    Console.WriteLine("\n\t 0 - Sair");
+                    Console.ResetColor();
+
+
+
+                    try
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.Write("\n\t Digite uma opção:");
+                        Console.ResetColor();
+                        escolha = int.Parse(Console.ReadLine());
+                        if (escolha < 0 || escolha > 5)
+                        {
+                            Console.Clear();
+                            Console.ForegroundColor = ConsoleColor.DarkYellow;
+                            Console.WriteLine("\n\t Opção inválida, tente novamente!");
+                            Console.WriteLine("\n\t Precione uma tecla para tentar novamente!");
+                            Console.ReadKey();
+                            Console.ResetColor();
+
+                        }
+                    }
+                    catch
+                    {
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.WriteLine("\n\t Opção inválida, tente novamente!");
+                        Console.WriteLine("\n\t Precione uma tecla para tentar novamente!");
+                        Console.ReadKey();
+                        Console.ResetColor();
+
+                    }
+
+                    switch (escolha)
+                    {
+                        case 1:
+                            Json.Remover();
+                            escolha = 0;
+                            break;
+
+                        case 0:
+
+                            break;
+                    }
+
+                } while (escolha != 0);
+
+            }
+            return nome;
+
+        }
+
+
+    }
+}
---
+++ Atividades/02 - Segunda fase (orientação a objeto)/Hub_de_jogos/teste02/Repositorio/Json.cs
@@ -0,0 +1,142 @@
+﻿
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using teste02.Entities;
+using JsonSerializer = Newtonsoft.Json.JsonSerializer;
+using Newtonsoft.Json;
+
+namespace teste02.Repositorio
+{
+    public class Json
+    {
+        
+        
+        private static string path = "Dados.json";
+
+        public static List<Jogador> jogadores = Desserializar();
+        public Json()
+        {
+            if (!File.Exists(path)) File.Create(path).Close();
+        }
+
+
+
+
+        public static void Adicionar()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("\n\t Cadastrar novo Jogador. ");
+            Console.ResetColor();
+
+            //pedir para o usuario escrever o nome 
+            string nome = Menu.PegarNome();
+
+            //validar o usuario e retornar o nome valido 
+            nome = Menu.ExisteJogador(nome);
+
+            if (Json.jogadores.Any(player => player.Nome == nome))
+            {
+                return;
+            }
+           
+
+            //criar um jogador utulizando a classe e o construtor 
+            Jogador jogador = new Jogador(nome);
+
+            //dicionar um jogador 
+            jogadores.Add(jogador);
+
+            //enviar para o json
+            Serializar();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("\n\t Jogador cadastrado com sucesso.");
+            Console.WriteLine("\n\t Pressione uma tecla para voltar ao menu");
+            Console.ReadKey();
+
+            Console.ResetColor();
+            
+        }
+        public static void Remover()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("\n\t Deletar Jogador.");
+            Console.ResetColor();
+            string nome = Menu.PegarNome();
+            nome = Menu.NaoExisteJogadorParaDeletar(nome);
+
+            if (!Json.jogadores.Any(player => player.Nome == nome))
+            {
+                return;
+            }
+
+            jogadores.RemoveAll(player => player.Nome == nome);
+            Serializar();
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("\n\t Jogador deletado com sucesso!");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("\n\t Pressione uma tecla para voltar ao menu");
+            Console.ReadKey();
+            Console.ResetColor();
+            return;
+
+        }
+
+       /* public static void Atualizar()
+        {
+            Console.Clear();
+            Console.ForegroundColor= ConsoleColor.DarkYellow;
+
+        }*/
+
+
+        public static void Serializar()
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(path))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    serializer.Serialize(sw, jogadores);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"\n\t{e.Message}");
+                Console.ResetColor();
+            }
+        }
+
+        public static List<Jogador> Desserializar()
+        {
+            List<Jogador> jogadores = new List<Jogador>();
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    string jsonString = sr.ReadToEnd();
+                    jogadores = JsonConvert.DeserializeObject<List<Jogador>>(jsonString);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"\n\t{e.Message}");
+                Console.ResetColor();
+            }
+            return jogadores;
+        }
+
+ 
+    }
+}
