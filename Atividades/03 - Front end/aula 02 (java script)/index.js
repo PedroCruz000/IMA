fbf345be6145e12284f7edacb6015516d105f13e
PedroCruz000 <112138392+PedroCruz000@users.noreply.github.com>
2023-02-13 22:50:58 -0300

    aula de js

---
+++ Atividades/03 - Front end/aula 02 (java script)/index.js
@@ -0,0 +1,48 @@
+
+
+
+
+//truthy and falsy values
+//truthy: != 0, "aaa"
+//falsy: null, undefined, [], "", NaN (not a number)
+
+//json:
+
+x = {
+
+   
+    nome: "joao",
+    idade: 20,
+    eltura:1.8,
+
+    dizerOi(nome){
+        //metodo
+        console.log(`olá, ${nome}`);
+    },
+
+};
+
+let a = 1
+b = 2
+
+// operador ternario 
+let autorizado = z.idade>= 18? "ok" : "not";
+
+//function
+
+const soma1 = (a,b) => a + b;
+const soma2 = function (a,b){
+    return a + b;
+}
+function soma3(a,b) {
+    return a + b;
+}
+
+
+
+
+document.getElementById("texto").innerText = "meu hello world";
+output.innerText += `x = ${x}`
+output.innerText += `x objeto = ${JSON.stringify(x)}`
+output.innerText= soma1
+console.log("ola mundo teste")
