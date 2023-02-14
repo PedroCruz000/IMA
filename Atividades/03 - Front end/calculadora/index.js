bb1aa674aef81f7d7e0b8e8d7707efb1a901b32e
PedroCruz000 <112138392+PedroCruz000@users.noreply.github.com>
2023-02-14 01:01:01 -0300

    iniciando js

--- Atividades/03 - Front end/calculadora/index.js
+++ Atividades/03 - Front end/calculadora/index.js
@@ -2,9 +2,49 @@ const previousOperationText = document.querySelector("#previus-operation")
 const currentOparationText = document.querySelector("#current-operation")
 const buttons = document.querySelectorAll(".buttons button")
 
+//objeto
 class Calculator{
+    constructor(previousOperationText,currentOparationText){
+        this.previousOperationText = previousOperationText
+        this.currentOparationText = currentOparationText
+        this.currentOparation = ""
+    }
+
+
+    //metodo para pegar o que o usuario esta digitando 
+    addDigit(digit){
+
+        //validando se ja existe uma virgula na operação para que não haja mais que um ponto.
+        if(digit === "," && this.currentOparationText.innerText.includes(",")){
+            return;
+        }
+
+        // adicionando o digito no visor 
+        this.currentOparation = digit
+        this.updateScreen()
+        
+    }
+   
+    // adicionando operações 
+    addOperations(operation){
+       this.currentOparation = operation
+       this.updateScreen()
+    }
+
+    // atualizar o visor 
+    updateScreen(){
+        this.currentOparationText.innerText += this.currentOparation;
+    }
 
 }
+
+
+
+
+
+// metodos
+const calc = new Calculator(previousOperationText, currentOparationText);
+
 
 buttons.forEach((btn) =>{
 
@@ -12,6 +52,11 @@ buttons.forEach((btn) =>{
 
         const value = e.target.innerText;
         
-        console.log(value);
+        if(+value >= 0 || value === ","){
+            calc.addDigit(value)
+        }else{
+            calc.addOperations(value)
+        }
+        
     })
 });
